ab116e3690967febde90e0c7b770abae2966f325
CalebHong <sstotss@naver.com>
2023-05-23 16:51:07 +0900

    Update ParaBolaMove.cs
    Add Lerp Drop

--- Utility/ParaBolaMove.cs
+++ Utility/ParaBolaMove.cs
@@ -7,6 +7,7 @@ public class ParaBolaMove : MonoBehaviour
     public float power;
 
     public float speed;
+    public float dropSpeed;
 
     private float _timer = 0;
 
@@ -21,18 +22,34 @@ public class ParaBolaMove : MonoBehaviour
     {
         _timer = 0;
 
+        Transform _transform = transform;
+
         while (true)
         {
-            _timer += speed * Time.deltaTime;
+            // 포물선 이동
+            if (_timer < 0.9f) 
+            {
+                _timer += speed * Time.deltaTime;
 
-            Vector3 MovePos = MoveParaBola(startPos, destPos, _timer);
+                Vector3 MovePos = MoveParaBola(startPos, destPos, _timer);
 
-            transform.position = MovePos;
+                _transform.position = MovePos;
 
-            Vector3 nextPos = MoveParaBola(startPos, destPos, _timer + 0.01f);
-            Vector3 forwardDir = (nextPos - MovePos).normalized;
+                Vector3 nextPos = MoveParaBola(startPos, destPos, _timer + 0.01f);
+                Vector3 forwardDir = (nextPos - MovePos).normalized;
 
-            transform.rotation = Quaternion.LookRotation(forwardDir, Vector3.up);
+                _transform.rotation = Quaternion.LookRotation(forwardDir, Vector3.up); 
+            }
+            // 점차 수직 낙하
+            else 
+            {
+                Vector3 targetRot = _transform.rotation.eulerAngles;
+                targetRot.x = Mathf.Lerp(targetRot.x, 90f, 0.05f);
+
+                Vector3 movePos = _transform.position + (dropSpeed * Time.deltaTime * _transform.forward);
+
+                _transform.SetPositionAndRotation(movePos, Quaternion.Euler(targetRot));
+            }
 
             yield return null;
         }
@@ -43,8 +60,9 @@ public class ParaBolaMove : MonoBehaviour
         float heightvalue = -power * height * _time * _time + power * height * _time;
 
         Vector3 pos = Vector3.Lerp(_start, _end, _time);
+        pos.y += heightvalue;
 
-        return new Vector3(pos.x, heightvalue + pos.y, pos.z);
+        return pos;
     }
 
     public void Stop()
