c71426a1f6463cdab43a352f5846f9f5d2b99185
CalebHong <sstotss@naver.com>
2024-02-17 23:31:24 +0900

    Add files via upload

---
+++ Utility/SerializableList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializableList<T>
+{
+    [SerializeField] List<T> List;
+
+    public List<T> ToList() { return List; }
+    public SerializableList(List<T> t) { List = t; }
+}
