ecb4cd173d1292169e7bb7b9f3207c2b537f3e22
CalebHong <sstotss@naver.com>
2024-03-21 16:55:57 +0900

    Add Default Value

--- Utility/ShuffleExtension.cs
+++ Utility/ShuffleExtension.cs
@@ -1,26 +1,36 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
-public static class ShuffleExtension 
+public static class ShuffleExtension
 {
-   public static void Shuffle<T> (this T[] array, int shuffleAccuracy) {
-      for (int i = 0; i < shuffleAccuracy; i++) {
-         int randomIndex = Random.Range (1, array.Length) ;
+    public static void Shuffle<T>(this T[] array, int shuffleAccuracy = -1)
+    {
+        if (shuffleAccuracy == -1)
+            shuffleAccuracy = array.Length;
 
-         T temp = array [ randomIndex ] ;
-         array [ randomIndex ] = array [ 0 ] ;
-         array [ 0 ] = temp ;
-      }
-   }
+        for (int i = 0; i < shuffleAccuracy; i++)
+        {
+            int randomIndex = Random.Range(1, array.Length);
 
-  
-   public static void Shuffle<T> (this List<T> list, int shuffleAccuracy) {
-      for (int i = 0; i < shuffleAccuracy; i++) {
-         int randomIndex = Random.Range (1, list.Count) ;
+            T temp = array[randomIndex];
+            array[randomIndex] = array[0];
+            array[0] = temp;
+        }
+    }
 
-         T temp = list [ randomIndex ] ;
-         list [ randomIndex ] = list [ 0 ] ;
-         list [ 0 ] = temp ;
-      }
-   }
+
+    public static void Shuffle<T>(this List<T> list, int shuffleAccuracy = -1)
+    {
+        if (shuffleAccuracy == -1)
+            shuffleAccuracy = list.Count;
+
+        for (int i = 0; i < shuffleAccuracy; i++)
+        {
+            int randomIndex = Random.Range(1, list.Count);
+
+            T temp = list[randomIndex];
+            list[randomIndex] = list[0];
+            list[0] = temp;
+        }
+    }
 }
