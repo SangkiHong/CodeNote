7c8b6d8cdf2daa9d1af4cd1dacb9f822465d2ea6
CalebHong <sstotss@naver.com>
2024-04-13 15:15:25 +0900

    Update InfiniteScroll.cs

--- Utility/InfiniteScroll.cs
+++ Utility/InfiniteScroll.cs
@@ -3,119 +3,116 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace ProjectD
+public class InfiniteScroll : MonoBehaviour
 {
-    public class InfiniteScroll : MonoBehaviour
+    [Header("Need")]
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private VerticalLayoutGroup verticalLayout;
+    [SerializeField] private RectTransform[] itemTransforms;
+
+    // 순서 인덱스 관리를 위한 인덱스 리스트(Bottom -> Top 순)
+    List<int> indexList = new List<int>();
+
+    private RectTransform contentTransform;
+
+    // 스크롤 감지 범위를 위한 2차원 벡터
+    private Vector2 scrollRange;
+    private float switchingSize;
+
+
+    private void Awake()
     {
-        [Header("Need")]
-        [SerializeField] private ScrollRect scrollRect;
-        [SerializeField] private VerticalLayoutGroup verticalLayout;
-        [SerializeField] private RectTransform[] itemTransforms;
+        contentTransform = scrollRect.content;
 
-        // 순서 인덱스 관리를 위한 인덱스 리스트(Bottom -> Top 순)
-        List<int> indexList = new List<int>();
+        for (int i = 0; i < itemTransforms.Length; i++)
+        { 
+            indexList.Add(i); 
+        }
 
-        private RectTransform contentTransform;
+        switchingSize = itemTransforms[0].sizeDelta.y + verticalLayout.spacing;
+        scrollRange.y = -switchingSize;
 
-        // 스크롤 감지 범위를 위한 2차원 벡터
-        private Vector2 scrollRange;
-        private float switchingSize;
+        MoveTo(100);
+    }
 
-
-        private void Awake()
+    private void FixedUpdate()
+    {
+        if (contentTransform != null)
         {
-            contentTransform = scrollRect.content;
+            float anchoredPosY = contentTransform.anchoredPosition.y;
+            if (anchoredPosY > 0)
+            {
+                scrollRect.movementType = ScrollRect.MovementType.Elastic;
+            }
+            else
+            {
+                if (scrollRect.movementType == ScrollRect.MovementType.Elastic)
+                    scrollRect.movementType = ScrollRect.MovementType.Unrestricted;
 
+                if (scrollRange.x < anchoredPosY && scrollRange.x < 0)
+                {
+                    TopToBottom();
+                }
+                else if (scrollRange.y > anchoredPosY)
+                {
+                    BottomToTop();
+                }
+            }
+        }
+    }
+
+    public void MoveTo(int focusIndex)
+    {
+        if (focusIndex > 3)
+        {
+            float adjustHeight = (focusIndex - 3) * switchingSize;
+            Vector2 setPoint = Vector2.up * -adjustHeight;
+            contentTransform.anchoredPosition = setPoint;
+
+            scrollRange.x = -adjustHeight;
+            scrollRange.y = -adjustHeight - switchingSize;
+
+            float bottomHeight = -2360 + adjustHeight;
             for (int i = 0; i < itemTransforms.Length; i++)
-            { 
-                indexList.Add(i); 
-            }
-
-            switchingSize = itemTransforms[0].sizeDelta.y + verticalLayout.spacing;
-            scrollRange.y = -switchingSize;
-
-            MoveTo(100);
-        }
-
-        private void FixedUpdate()
-        {
-            if (contentTransform != null)
             {
-                float anchoredPosY = contentTransform.anchoredPosition.y;
-                if (anchoredPosY > 0)
-                {
-                    scrollRect.movementType = ScrollRect.MovementType.Elastic;
-                }
-                else
-                {
-                    if (scrollRect.movementType == ScrollRect.MovementType.Elastic)
-                        scrollRect.movementType = ScrollRect.MovementType.Unrestricted;
-
-                    if (scrollRange.x < anchoredPosY && scrollRange.x < 0)
-                    {
-                        TopToBottom();
-                    }
-                    else if (scrollRange.y > anchoredPosY)
-                    {
-                        BottomToTop();
-                    }
-                }
-            }
+                Vector2 setPos = itemTransforms[i].anchoredPosition;
+                setPos.y = bottomHeight + (i * switchingSize);
+                itemTransforms[i].anchoredPosition = setPos;
+            } 
         }
+    }
 
-        public void MoveTo(int focusIndex)
+
+    private void BottomToTop()
+    {
+        scrollRange.x -= switchingSize;
+        scrollRange.y -= switchingSize;
+
+        int bottomIndex = indexList.First();
+        int topIndex = indexList.Last();
+
+        Vector2 anchoredPos = itemTransforms[topIndex].anchoredPosition;
+        anchoredPos.y += switchingSize;
+        itemTransforms[bottomIndex].anchoredPosition = anchoredPos;
+        indexList.RemoveAt(0);
+        indexList.Add(bottomIndex);
+    }
+
+    private void TopToBottom()
+    {
+        if (scrollRange.x < 0)
         {
-            if (focusIndex > 3)
-            {
-                float adjustHeight = (focusIndex - 3) * switchingSize;
-                Vector2 setPoint = Vector2.up * -adjustHeight;
-                contentTransform.anchoredPosition = setPoint;
-
-                scrollRange.x = -adjustHeight;
-                scrollRange.y = -adjustHeight - switchingSize;
-
-                float bottomHeight = -2360 + adjustHeight;
-                for (int i = 0; i < itemTransforms.Length; i++)
-                {
-                    Vector2 setPos = itemTransforms[i].anchoredPosition;
-                    setPos.y = bottomHeight + (i * switchingSize);
-                    itemTransforms[i].anchoredPosition = setPos;
-                } 
-            }
+            scrollRange.x += switchingSize;
+            scrollRange.y += switchingSize;
         }
 
+        int bottomIndex = indexList.First();
+        int topIndex = indexList.Last();
 
-        private void BottomToTop()
-        {
-            scrollRange.x -= switchingSize;
-            scrollRange.y -= switchingSize;
-
-            int bottomIndex = indexList.First();
-            int topIndex = indexList.Last();
-
-            Vector2 anchoredPos = itemTransforms[topIndex].anchoredPosition;
-            anchoredPos.y += switchingSize;
-            itemTransforms[bottomIndex].anchoredPosition = anchoredPos;
-            indexList.RemoveAt(0);
-            indexList.Add(bottomIndex);
-        }
-
-        private void TopToBottom()
-        {
-            if (scrollRange.x < 0)
-            {
-                scrollRange.x += switchingSize;
-                scrollRange.y += switchingSize;
-            }
-
-            int bottomIndex = indexList.First();
-            int topIndex = indexList.Last();
-
-            Vector2 anchoredPos = itemTransforms[bottomIndex].anchoredPosition;
-            anchoredPos.y -= switchingSize;
-            itemTransforms[topIndex].anchoredPosition = anchoredPos;
-            indexList.RemoveAt(indexList.Count - 1);
-            indexList.Insert(0, topIndex);
-        }
+        Vector2 anchoredPos = itemTransforms[bottomIndex].anchoredPosition;
+        anchoredPos.y -= switchingSize;
+        itemTransforms[topIndex].anchoredPosition = anchoredPos;
+        indexList.RemoveAt(indexList.Count - 1);
+        indexList.Insert(0, topIndex);
     }
 }
