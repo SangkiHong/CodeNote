1415ea0251ab2519bbb51fa79eac165076d5a857
CalebHong <sstotss@naver.com>
2024-08-26 17:11:35 +0900

    Update AdjustRotation.cs

--- Utility/AdjustRotation.cs
+++ Utility/AdjustRotation.cs
@@ -21,8 +21,7 @@ private float ArrangeRotationValue(float value)
 /// </summary>
 private float AdjustAngle(float angle, float roundAngle)
 {
-    int value = Mathf.RoundToInt(angle);
-    int meta = Mathf.FloorToInt(value / roundAngle) + (value > 0 ? 1 : -1);
+    int meta = Mathf.FloorToInt(angle / roundAngle) + (angle > 0 ? 1 : -1);
     meta = Mathf.FloorToInt(meta / 2);
     return Mathf.Round(meta * (roundAngle * 2));
 }
