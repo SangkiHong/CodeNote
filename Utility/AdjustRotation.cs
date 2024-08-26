a7196bbaadcd7ccbffb4d459a0cd47a2dfceef33
CalebHong <sstotss@naver.com>
2024-08-26 17:00:07 +0900

    Create AdjustRotation.cs

---
+++ Utility/AdjustRotation.cs
@@ -0,0 +1,28 @@
+/// <summary>
+/// Adjusts the value to be within the range of -180 to 180.
+/// </summary>
+private float ArrangeRotationValue(float value)
+{
+    if (value > 180f)
+    {
+        value -= 360f;
+    }
+    else if (value < -180f)
+    {
+        value += 360f;
+    }
+
+    return value;
+}
+
+/// <summary>
+/// Adjusts the angle to 0 degrees if it is less than or equal to 45 degrees, 
+/// or to 90 degrees if it is greater than 45 degrees.
+/// </summary>
+private float AdjustAngle(float angle, float roundAngle)
+{
+    int value = Mathf.RoundToInt(angle);
+    int meta = Mathf.FloorToInt(value / roundAngle) + (value > 0 ? 1 : -1);
+    meta = Mathf.FloorToInt(meta / 2);
+    return Mathf.Round(meta * (roundAngle * 2));
+}
